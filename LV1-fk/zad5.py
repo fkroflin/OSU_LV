dc39dcf498a27658ea6aa06ad84bc33c54696beb
Fran <87648099+fkroflin@users.noreply.github.com>
2024-03-14 14:20:04 +0100

    Update zad5.py

--- LV1-fk/zad5.py
+++ LV1-fk/zad5.py
@@ -4,9 +4,9 @@ hamAvg = 0.0
 spamAvg = 0.0
 spamExclamation = 0
 
-file = open('SMSSpamCollection.txt')
+fhand = open('SMSSpamCollection.txt')
 
-for line in file:
+for line in fhand:
     line = line.rstrip().split()
     if line[0] == 'spam':
         spam += 1
@@ -15,7 +15,7 @@ for line in file:
     else:
         ham += 1
         hamAvg += len(line[1:])
-file.close()
+fhand.close()
 
 hamAvg /= ham
 spamAvg /= spam
