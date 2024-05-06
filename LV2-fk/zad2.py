27bb42cca09633617551762148ada88375520cc3
Fran <87648099+fkroflin@users.noreply.github.com>
2024-05-06 21:19:15 +0200

    Updated

--- LV2-fk/zad2.py
+++ LV2-fk/zad2.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 from numpy import genfromtxt
 
 #a
-file = genfromtxt('data.csv', delimiter=',')
-print("Broj osoba na koliko je izvrseno mjerenje:", len(file))
+file = np.loadtxt("data.csv", delimiter=',', skiprows=1)
+print("Broj osoba na koliko je izvrseno mjerenje: ",len(file))
 
 #b
-x=np.array(file[1:,2])
-y=np.array(file[1:,1])
+x=np.array(file[:,2])
+y=np.array(file[:,1])
 plt.scatter(x,y)
 plt.xlabel("masa(kg)")
 plt.ylabel("visina(cm)")
@@ -16,8 +16,8 @@ plt.title("Odnos visine i mase natjecatelja")
 # plt.show()
 
 #c
-x1=np.array(file[1::50,2])
-y1=np.array(file[1::50,1])
+x1=np.array(file[::50,2])
+y1=np.array(file[::50,1])
 plt.scatter(x1,y1)
 plt.xlabel("masa(kg)")
 plt.ylabel("visina(cm)")
@@ -32,7 +32,6 @@ print("Srednja vrijednost visine:", y.mean(), "cm")
 #e
 men=file[np.where(file[:,0]==1)]
 women=file[np.where(file[:,0]==0)]
-print("Najmanja visina kod muskaraca:", men[1:,1].min(), "cm", "a kod zena:", women[1:,1].min(), "cm")
-print("Najvisa visina kod muskaraca:", men[1:,1].max(), "cm", "a kod zena:", women[1:,1].max(), "cm")
-print("Srednja visina kod muskaraca:", men[1:,1].mean(), "cm", "a kod zena:", women[1:,1].mean(), "cm")
-
+print("Najmanja visina kod muskaraca:", men[:,1].min(), "cm", "a kod zena:", women[:,1].min(), "cm")
+print("Najvisa visina kod muskaraca:", men[:,1].max(), "cm", "a kod zena:", women[:,1].max(), "cm")
+print("Srednja visina kod muskaraca:", men[:,1].mean(), "cm", "a kod zena:", women[:,1].mean(), "cm")
